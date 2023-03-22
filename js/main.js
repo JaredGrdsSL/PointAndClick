918545eaef80f71fbc59c716c82059fe5d1a307f
Qwerzer23 <127936920+JaredGrdsSL@users.noreply.github.com>
2023-03-22 12:22:05 +0100

    1

--- js/main.js
+++ js/main.js
@@ -7,11 +7,25 @@ const gameWindow = document.getElementById('gameWindow');
 
 const playerOffset = 8;
 
-gameWindow.onclick = function(e){
+gameWindow.onclick = function (e) {
     var rect = gameWindow.getBoundingClientRect();
     var x = e.clientX - rect.left; // mouse x
-    var y = e.clientY - rect.top ;  // mouse y
+    var y = e.clientY - rect.top;  // mouse y
     player.style.left = x - playerOffset + "px";
     player.style.top = y - playerOffset + "px";
-    console.log(x);
+
+    // console.log(e.target.id);
+
+    switch (e.target.id) {
+        case "door1":
+            console.log("*garbled nonsense*");
+            break;
+        case "door2":
+            console.log("DON'T come in. PRIVATE property!");
+            break;
+        default:
+            console.log("Nothing important");
+            break;
+    }
+    // console.log(x);
 }
