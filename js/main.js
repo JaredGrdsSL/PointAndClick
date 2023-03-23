dd5e134783c72b263582bfb560dc1ad1cd5876e1
Qwerzer23 <127936920+JaredGrdsSL@users.noreply.github.com>
2023-03-23 15:21:14 +0100

    1

--- js/main.js
+++ js/main.js
@@ -1,4 +1,4 @@
-document.getElementById('mainTitle').innerText = "Point and click adventure";
+document.getElementById('mainTitle').innerText = "Haunt 'em John!";
 document.getElementById('tilemapImg').setAttribute('draggable', false);
 document.getElementById('playerImg').setAttribute('draggable', false);
 
@@ -6,7 +6,10 @@ const player = document.getElementById('player');
 const gameWindow = document.getElementById('gameWindow');
 const door1Audio = document.getElementById('door1Audio');
 const door2Audio = document.getElementById('door2Audio');
+const dialogueBox = document.getElementById('dialogueBox');
 const playerOffset = 8;
+
+dialogueBox.classList.add("disabled");
 
 gameWindow.onclick = function (e) {
     var rect = gameWindow.getBoundingClientRect();
@@ -14,18 +17,27 @@ gameWindow.onclick = function (e) {
     var y = e.clientY - rect.top;  // mouse y
     player.style.left = x - playerOffset + "px";
     player.style.top = y - playerOffset + "px";
+    dialogueBox.classList.remove("typed");
 
     switch (e.target.id) {
         case "door1":
+            dialogueBox.classList.remove("disabled");
+            dialogueBox.classList.add("typed");
             door1Audio.play();
-            console.log("*garbled nonsense*");
+            showSpeech("(You hear garbled nonsense)")
             break;
         case "door2":
+            dialogueBox.classList.remove("disabled");
+            dialogueBox.classList.add("typed");
             door2Audio.play();
-            console.log("DON'T come in. PRIVATE property!");
+            showSpeech("(You feel an overbearing presence telling you not to go in.)")
             break;
         default:
-            console.log("Nothing important");
+            dialogueBox.classList.add("disabled");
             break;
     }
+}
+
+function showSpeech(dialogue){
+    dialogueBox.innerText = dialogue;
 }
